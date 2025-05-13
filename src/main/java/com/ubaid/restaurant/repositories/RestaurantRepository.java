064f73c9d73ef6b4ea9f07f2017bd61651563326
ubaidnaveed <ubaidub97@gmail.com>
2025-05-14 00:06:56 +0500

    Created all the required entities and Restaurant repository.

---
+++ src/main/java/com/ubaid/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package com.ubaid.restaurant.repositories;
+
+import com.ubaid.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+}
