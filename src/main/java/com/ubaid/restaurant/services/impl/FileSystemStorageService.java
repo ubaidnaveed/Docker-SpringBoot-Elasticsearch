8748036c47c80b154911dc9ff840aa9bb3ca3c82
ubaidnaveed <ubaidub97@gmail.com>
2025-05-15 23:30:36 +0500

    Storing and Retrieving Files.

---
+++ src/main/java/com/ubaid/restaurant/services/impl/FileSystemStorageService.java
@@ -0,0 +1,87 @@
+package com.ubaid.restaurant.services.impl;
+
+import com.ubaid.restaurant.exceptions.StorageException;
+import com.ubaid.restaurant.services.StorageService;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+@Service
+@Slf4j
+public class FileSystemStorageService implements StorageService {
+
+    @Value("${app.storage.location:uploads}")
+    private String storageLocation;
+
+    private Path rootLocation;
+
+    @PostConstruct
+    public void init(){
+        rootLocation = Paths.get(storageLocation);
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not initialize storage location",e);
+        }
+    }
+
+    @Override
+    public String store(MultipartFile file, String filename) {
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Cannot save an empty file.");
+            }
+            String extention = StringUtils.getFilenameExtension(file.getOriginalFilename());
+            String finalFileName = filename + "." + extention;
+
+            Path destinationFile = rootLocation
+                    .resolve(Paths.get(finalFileName))
+                    .normalize()
+                    .toAbsolutePath();
+
+            if (!destinationFile.getParent().equals(rootLocation.toAbsolutePath())) {
+                throw new StorageException("Can not save file in an another directory.");
+            }
+
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, destinationFile, StandardCopyOption.REPLACE_EXISTING);
+            }
+
+            return finalFileName;
+        } catch (IOException e){
+            throw new StorageException("Couldn't initialize storage location", e);
+        }
+    }
+
+    @Override
+    public Optional<Resource> loadAsResource(String filename) {
+        try{
+            Path file = rootLocation.resolve(filename);
+
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()){
+                return Optional.of(resource);
+
+            }else {
+                return Optional.empty();
+            }
+        }catch (MalformedURLException e){
+            log.warn("Could not read file:%s".formatted(filename), e);
+            return Optional.empty();
+        }
+    }
+}
