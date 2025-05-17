6ae4ea6706257a1cf7c822571664b4ec3fa01b88
ubaidnaveed <ubaidub97@gmail.com>
2025-05-17 12:54:49 +0500

    Photo upload and retrieval endpoints.

---
+++ src/main/java/com/ubaid/restaurant/controllers/PhotoController.java
@@ -0,0 +1,40 @@
+package com.ubaid.restaurant.controllers;
+
+import com.ubaid.restaurant.domain.dtos.PhotoDto;
+import com.ubaid.restaurant.domain.entities.Photo;
+import com.ubaid.restaurant.mappers.PhotoMapper;
+import com.ubaid.restaurant.services.PhotoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/photos")
+public class PhotoController {
+    private final PhotoService photoService;
+    private final PhotoMapper photoMapper;
+
+    @PostMapping
+    public PhotoDto uploadPhoto(@RequestParam("file")MultipartFile file){
+        Photo savedPhoto = photoService.uploadPhoto(file);
+        return photoMapper.toDto(savedPhoto);
+    }
+
+    @GetMapping(path = "/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id){
+        return photoService.getPhotoAsResource(id).map(photo -> ResponseEntity.ok()
+                .contentType(
+                        MediaTypeFactory.getMediaType(photo)
+                        .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                )
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                .body(photo)
+        ).orElse(ResponseEntity.notFound().build());
+    }
+}
